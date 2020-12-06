188ebb4713de58f0993051e07179ca5d8c5d4140
Yan Lion <ylion75@gmail.com>
2020-12-06 11:39:53 +0100

    Mise en place test Plateau

---
+++ src/PlateauJeu.java
@@ -0,0 +1,17 @@
+public class PlateauJeu {
+    private int rangee;
+    private int trou; //correspond à une case
+    private int graine;
+
+    public PlateauJeu(){
+        //les données sont mises en dur pour l'instant
+        this.rangee = 2;
+        this.trou = 6;
+        this.graine = 24;
+    }
+
+    public static int
+
+    //a faire:
+    //afficher grille
+}
