84def7615a826669f91c62a3730269d062a4de74
Yan Lion <ylion75@gmail.com>
2020-12-06 20:29:53 +0100

    Ajout constructeur vide, Il faut sortir l'affichage du constructeur

--- src/Appli.java
+++ src/Appli.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
+
 public class Appli {
     public static void main(String[] args) {
-        PlateauJeu awalé = new PlateauJeu(2, 6);
-        //awalé.toString();
+        //Sans paramètre, il lance l'awalé de base.
+        PlateauJeu awalé2 = new PlateauJeu();
+        //System.out.println(awalé2.toString());
 
+        //version custom, pour les autres versions du jeu
+        PlateauJeu awalé = new PlateauJeu(2, 6);
+        //System.out.println(awalé.toString());
 
     }
 }
--- src/PlateauJeu.java
+++ src/PlateauJeu.java
@@ -1,21 +1,22 @@
 import java.util.Arrays;
+import java.util.Arrays.*;
 import java.io.*;
 
 public class PlateauJeu {
     private int rangee;
     private int trou; //correspond à une case
-    private int graine;
+    private int graineParTrou;
     private int[][] plateau;
 
     public PlateauJeu(int rangee, int trou){
         //les données sont mises en dur pour l'instant
         this.rangee = rangee;
         this.trou = trou;
-
+        this.graineParTrou = 4;
         plateau  = new int[rangee][trou];
         for (int i = 0;i < plateau.length; i++) {
             for (int j = 0;j < plateau[i].length;j++) {
-                plateau[i][j] = 4;
+                plateau[i][j] = graineParTrou;
                 System.out.print(plateau[i][j] + " ");
             }
             System.out.println();
@@ -24,26 +25,26 @@ public class PlateauJeu {
 
     }
 
+    public PlateauJeu(){
+        this.rangee = 2;
+        this.trou = 6;
+        this.graineParTrou = 4;
+    }
 
+    //pb avec toString parce qu'il faut convertir le tableau en String
+    //pour l'affichage
     /*
-    public void init(){
-        plateau  = new int[2][6];
-        for (int i = 0;i < plateau.length; i++) {
-            for (int j = 0;j < plateau[i].length;j++) {
-                plateau[i][j] = 4;
-                System.out.print(plateau[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
+    @Override
+    public String toString() {
+        return;
+
+
+    }
 
      */
+
+
 }
 
 
-
-    //public static int
-
-    //a faire:
-    //afficher grille
 
