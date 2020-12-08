6f5a3732400d3f6c2c88d2fcd7b04da837ba066a
Simtim <simon.timmerman@outlook.com>
2020-12-08 23:23:05 +0100

    Affichage "graphique" fini

--- src/PlateauJeu.java
+++ src/PlateauJeu.java
@@ -10,43 +10,63 @@ public class PlateauJeu {
     private int[][] plateau;
 
 
-    public PlateauJeu(int rangee, int trou){
+    public PlateauJeu(int rangee, int trou) {
         //les données sont mises en dur pour l'instant
         this.rangee = rangee;
         this.trou = trou;
         this.graineParTrou = 4;
-        this.plateau  = new int[rangee][trou];
-        for (int i = 0;i < this.plateau.length; i++) {
-            for (int j = 0;j < this.plateau[i].length;j++) {
+        this.plateau = new int[rangee][trou];
+        for (int i = 0; i < this.plateau.length; i++) {
+            for (int j = 0; j < this.plateau[i].length; j++) {  //Intellij sugere Arrays.fill(this.plateau[i], this.graineParTrou);
                 this.plateau[i][j] = this.graineParTrou;
             }
         }
     }
 
-    public PlateauJeu(){
+    public PlateauJeu() {
         this.rangee = 2;
         this.trou = 6;
         this.graineParTrou = 4;
     }
 
     //pb avec toString parce qu'il faut convertir le tableau en String
+
+
     //pour l'affichage
 
     public void afficherPlateau() {
-        for (int i = this.rangee * this.trou; i > this.trou; i--){
-            System.out.print("  " + i + "  ");
+        int cpt = 0;
+        System.out.print("NORD");
+        for (int i = this.rangee * this.trou; i > this.trou; i--) { // NORD
+            if (i >= 10) { //Permet d'augmenter l'espace quand un chiffre contient moins de deux nombres
+                System.out.print("  " + i + "  ");
+            } else {
+                System.out.print("  " + i + "   ");
+            }
         }
         System.out.println(); //saut de ligne
-
-        for (int[] ints : plateau) {
-            for (int anInt : ints) {
-                System.out.print("[ " + anInt + " ] ");
-
+        System.out.print("    ");
+            for (int[] ints : plateau) {
+                for (int anInt : ints) {
+                    System.out.print("[ " + anInt + " ] ");
+                    cpt ++;
+                }
+                if (cpt == 6) {    // condition qui permet de dire qu'au 6e [4] faire un saut de ligne plus espace
+                    System.out.println();    //sans ça SUD était décallé au niveau du premier 4
+                    System.out.print("    ");
+                }
+                else{
+                    System.out.println();
+                }
             }
-            System.out.println();
+        System.out.print("SUD ");
+            for (int j = 1; j <= this.trou; j++) { //Ligne SUD
+                System.out.print("  " + j + "   ");
+            }
+        System.out.println(); //saut de ligne
         }
     }
-}
+
 
 
 
