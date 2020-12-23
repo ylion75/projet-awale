8f015402edc82c4dc36a7021ddc7238a45beed9d
Yan Lion <ylion75@gmail.com>
2020-12-23 11:11:25 +0100

    Tests OK pour PlateauJeu en mode normal et avec le constructeur vide

--- test/PlateauJeuTest.java
+++ test/PlateauJeuTest.java
@@ -5,9 +5,16 @@ import static org.junit.Assert.assertEquals;
 public class PlateauJeuTest {
     @Test
     public void PlateauJeuTest(){
-        PlateauJeu p = new PlateauJeu(2,6);
-        assertEquals(p.getRangee(), 2);
-        assertEquals(p.getTrou(),6);
+        PlateauJeu p = new PlateauJeu(3,5, 8);
+        assertEquals(p.getRangee(), 3);
+        assertEquals(p.getTrou(),5);
+        assertEquals(p.getGraineDansTrou(1,1),8);
+
+        PlateauJeu p2 = new PlateauJeu();
+        assertEquals(p2.getRangee(), 2);
+        assertEquals(p2.getTrou(), 6);
+        assertEquals(p2.getGraineDansTrou(1,1),4);
     }
+
 
 }
