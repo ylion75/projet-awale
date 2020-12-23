933ed84a0260b351d17cad0d754d3381ed4d7757
Yan Lion <ylion75@gmail.com>
2020-12-23 16:33:15 +0100

    Ajout tests premierJoueurTest() et joueurSuivantTest();

--- test/JeuTest.java
+++ test/JeuTest.java
@@ -1,2 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class JeuTest {
+    @Test
+    public void premierJoueurTest(){
+        Jeu awale = new Jeu();
+        assertEquals(awale.premierJoueur(), 0);
+    }
+
+    @Test
+    public void joueurSuivantTest(){
+        Jeu awale = new Jeu();
+        assertEquals(awale.joueurSuivant(1),0);
+        assertEquals(awale.joueurSuivant(0),1);
+    }
 }
