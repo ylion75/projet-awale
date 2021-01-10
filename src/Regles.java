0d4359d4c48d8e7af8f2cff7ee4561f2d7696c8d
Yan Lion <ylion75@gmail.com>
2021-01-10 13:52:21 +0100

    Update règles

--- src/Regles.java
+++ src/Regles.java
@@ -1,11 +1,9 @@
 public interface Regles {
     //L'interface permet de spécifier les règles, elle doit permettre les extensions futures et notamment l'ajout de règles supplémentaires.
-    //Les méthodes sont publiques et non définies
-
+    
     public abstract Joueur premierJoueur();
     public abstract void jouerUnCoup(Joueur joueurActif, int choixUtilisateur);
     public abstract void ramasserGraine(int numeroJoueur, int numeroTrou, Joueur joueurActif);
     public abstract int[] semer(int numeroJoueur, int numeroTrou);
     public abstract void deroulePartie();
-
 }
